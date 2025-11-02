7b1c8a695150a8935b8df582893a8b232a44a346
ǝ-meer <amirkerman055@gmail.com>
2025-11-02 15:47:09 +0330

    Add files via upload

---
+++ project2-tamrin1.py
@@ -0,0 +1,51 @@
+a=True
+b=False
+c=8
+d=3
+e=-10
+f=3.14
+g="amir"
+h="amin"
+i=1679
+j=[6,8,12]
+
+print(a and c)
+print(c and e)
+print(b and c)
+print(d and b)
+print(e and b)
+print(a and b)
+print(i and j)
+print(h and g)
+print(j and i)
+print(g and h)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(a or b)
+print(b or a)
+print(c or d)
+print(d or c)
+print(g or h)
+print(h or g)
+print(i or j)
+print(j or i)
+print(f or e)
+print(b or d)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(not a)
+print(not b)
+print(not c)
+print(not d)
+print(not e)
+print(not f)
+print(not g)
+print(not h)
+print(not i)
+print(not j)
+
+
+
+
---
+++ project2-tamrin2.py
@@ -0,0 +1,22 @@
+a=24
+b=18.89
+c=1404
+d=True
+e=False
+f=a
+g=2077
+h="amir"
+i=c
+j=21
+
+print(a is b)
+print(c is d)
+print(c is e)
+print(f is g)
+print(h is j)
+print(i is False)
+print(a is not c)
+print(type(c) is list)
+print(id(c)==id(e))
+print(id(c)==id(d))
+      
