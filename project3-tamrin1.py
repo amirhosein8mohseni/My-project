8a129f711fe3abf3f225c86e34d4961592117aca
ǝ-meer <amirkerman055@gmail.com>
2025-11-07 22:11:07 +0330

    Add files via upload

---
+++ project3-tamrin1.py
@@ -0,0 +1,51 @@
+#Function One 
+def daf(x):
+    y = 5
+    return y
+
+#Functin Two
+def tabe(x):
+    y = x
+    return y
+
+#Function Three
+def zarb(x, a, b, c):
+    y = a * x**2 + b * x + c
+    return y
+
+#Function Four
+def amir(x, a, b, c, d):
+    y = a * x**3 + b * x**2 + c * x + d
+    return y
+
+#Function Five
+def tavan(a, x):
+    y = a ** x
+    return y
+
+#Function six
+def negetive(x):
+    y = -x
+    return y
+
+#Fnction seven
+def kasr(a, d):
+    y = a - (d / 7)
+    return y
+
+#Function eigt
+def moraba(a, b, x, z):
+    y = a * x + b * z
+    return y
+
+
+
+
+
+
+
+
+
+
+
+
