48af28c2091c807b8408ad9bcd5bb58910604b42
ǝ-meer <amirkerman055@gmail.com>
2025-11-23 20:09:08 +0330

    Add files via upload

---
+++ project5-tamrin1.py
@@ -0,0 +1,216 @@
+#توابع بازگشتی
+def tabe(x):
+    print(x)
+    if (x>=10):
+        return 10
+    else:
+        return tabe(x+1) 
+    
+input("برای ادامه Enterفشار دهید.")
+
+#عبارات منطقی
+a=True
+b=False
+c=14
+d=99
+e=-8
+f=3.14
+g="amir"
+h="amin"
+i=1383
+j=[12,2,4]
+
+print(a and c)
+print(c and e)
+print(b and c)
+print(d and b)
+print(e and b)
+print(a and b)
+print(i and j)
+print(h and g)
+print(j and i)
+print(g and h)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(a or b)
+print(b or a)
+print(c or d)
+print(d or c)
+print(g or h)
+print(h or g)
+print(i or j)
+print(j or i)
+print(f or e)
+print(b or d)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(not a)
+print(not b)
+print(not c)
+print(not d)
+print(not e)
+print(not f)
+print(not g)
+print(not h)
+print(not i)
+print( not j)
+
+input("برای ادامه Enterفشار دهید.")
+#عبارات محاسباتی
+a=13
+b=134
+c=14
+d=99
+e=-8
+f=3.14
+g=6755
+h=3856
+i=1383
+j=1224
+k=2345
+l=256
+m=456
+n=35
+o=265
+p=895
+q=456
+r=76235
+s=536
+t=456
+u=346
+v=3456
+
+print(a+j)
+print(b+i)
+print(c+h)
+print(d+g)
+print(e+f)
+print(f+e)
+print(g+d)
+print(h+c)
+print(i+b)
+print(j+a)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(a-j)
+print(b-i)
+print(c-h)
+print(d-g)
+print(e-f)
+print(f-e)
+print(g-d)
+print(h-c)
+print(i-b)
+print(j-a)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(j*a)
+print(j*b)
+print(j*c)
+print(j*d)
+print(j*e)
+print(j*f)
+print(j*g)
+print(j*h)
+print(j*i)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(a**j)
+print(b**i)
+print(c**h)
+print(d**g)
+print(e**f)
+print(f**e)
+print(g**d)
+print(h**c)
+print(i**b)
+print(j**a)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(j/a)
+print(j/b)
+print(j/c)
+print(j/d)
+print(j/e)
+print(j/f)
+print(j/g)
+print(j/h)
+print(j/i)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(a%j)
+print(b%i)
+print(c%h)
+print(d%g)
+print(e%f)
+print(f%e)
+print(g%d)
+print(h%c)
+print(i%b)
+print(j%a)
+
+input("برای ادامه Enterفشار دهید.")
+
+print(a//j)
+print(b//i)
+print(c//h)
+print(d//g)
+print(e//f)
+print(f//e)
+print(g//d)
+print(h//c)
+print(i//b)
+print(j//a)
+
+input("برای ادامه Enterفشار دهید.")
+
+#عبارات مقایسه ای
+print(j>a)
+print(j>b)
+print(j<c)
+print(j>d)
+print(j<e)
+print(j<f)
+print(j<g)
+print(j>h)
+print(j<i)
+
+print(k>=a)
+print(l<=b)
+print(m>=c)
+print(o<=d)
+print(p>=e)
+print(q<=f)
+print(r>=g)
+print(s<=h)
+print(t>=i)
+print(u<=j)
+
+print(j!=k)
+print(i==l)
+print(h!=m)
+print(g==o)
+print(f!=p)
+print(e==q)
+print(d!=v)
+print(b!=n)
+print(c==t)
+print(a==u)
+
+
+
+
+
+
+
+
+
+
+
